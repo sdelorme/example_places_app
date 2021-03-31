547b104aa21032381feac2ad38877fc4389bf648
Seth Delorme <delorme.s.m@gmail.com>
2021-03-30 20:49:48 -0500

    seeds db

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+# place = Place.new(name: "Rock of Ages Granite Quarry", address: "555 Quarry Dr., Barre, VT")
+# place.save
+# place = Place.new(name: "World's Talles Ladderback Chair", address: "123 Chair Ln, Bennington, VT")
+# place.save
+# place = Place.new(name: "Monument to a Lake Monster", address: "999 Lake Front Dr., Burlington, VT")
+# place.save
+# place = Place.new(name: "Queen Connie Statue", address: "555 Route 7, Leicester, VT")
+# place.save
